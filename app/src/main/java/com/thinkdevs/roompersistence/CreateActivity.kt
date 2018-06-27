aca29490a9ccc1a0fc53266f7d354bc29473b6e1
Kevin Janvier <kevin.janvier5@gmail.com>
2018-06-27 21:02:15 +0300

    add create activity

---
+++ app/src/main/java/com/thinkdevs/roompersistence/CreateActivity.kt
@@ -0,0 +1,13 @@
+package com.thinkdevs.roompersistence
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class CreateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_activty)
+    }
+}
